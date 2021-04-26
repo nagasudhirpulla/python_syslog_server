182a3a3db8eefbd4b9b2e8681a235c80cd343660
unknown <nagasudhirpulla@gmail.com>
2021-04-26 11:03:10 +0530

    zip logging implemented

--- src/loggerFactory.py
+++ src/loggerFactory.py
@@ -1,6 +1,13 @@
 import logging
 from logging.handlers import TimedRotatingFileHandler
 from logging import LoggerAdapter
+import os
+import zipfile
+from os.path import basename
+
+def rotator(source, dest):
+    zipfile.ZipFile(dest, 'w', zipfile.ZIP_DEFLATED).write(source, basename(source))
+    os.remove(source)
 
 
 def getFileLogger(name: str, fPath: str, backupCount: int, numRollingHrs: int) -> LoggerAdapter:
@@ -9,7 +16,8 @@ def getFileLogger(name: str, fPath: str, backupCount: int, numRollingHrs: int) -
     # streamHandler = logging.StreamHandler()
     fileHandler = TimedRotatingFileHandler(
         fPath, backupCount=backupCount, when='h', interval=numRollingHrs)
-    fileHandler.namer = lambda name: name.replace(".log", "") + ".log"
+    fileHandler.namer = lambda name: name.replace(".log", "") + ".zip"
+    fileHandler.rotator = rotator
     streamFormatter = logging.Formatter("%(message)s")
     # streamHandler.setFormatter(streamFormatter)
     fileHandler.setFormatter(streamFormatter)
